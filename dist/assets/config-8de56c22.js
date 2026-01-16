04dd8236ef04deac3fe058ffa577f656e8de9e0c
Jan <893753780@qq.com>
2026-01-16 09:45:46 +0800

    删除无用的包

--- dist/assets/config-8de56c22.js
+++
@@ -1,1 +0,0 @@
-const E=[10,20,30,50,100],s={SLOW:2e3,NORMAL:1e3,FAST:500};export{s as D,E as P};
--- dist/assets/regex-a1235873.js
+++
@@ -1,1 +0,0 @@
-const E=new RegExp(/^[\d]+$/),R=new RegExp(/^\d+(\.\d{1,2})?$/);export{E as N,R as P};
